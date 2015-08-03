9cb903fca7fecf6f3545d5993ed2a6c7cda5e170
m6baymax <liujiangfeng007@gmail.com>
2015-08-03 18:34:58 +0800

    register event

--- v1/work/javascripts/index.js
+++ v1/work/javascripts/index.js
@@ -95,3 +95,143 @@ function uploadimg( option , success , error) {
         $( '#'+file.id ).find('.progress').remove();
     });
 }
+
+
+ $(function() {
+
+    // 设置根字体大小            
+    setRootFontSize = function() {
+        var ww = $(".worktai").width();
+        var root;
+
+        if (ww < 320) root = 50;
+        else if (320 <= ww && ww < 640) root = ww * 100 / 640;
+        else if (640 <= ww) root = 100;
+
+        $("html").css("font-size", root + "px");
+    }
+    
+    $(document).resize(setRootFontSize).load(setRootFontSize).resize();
+
+    function clickRegisterButton(){
+        $(".right-side .pop-window").removeClass("hidden");
+        $(".but1").click(function(){
+            $(".pop").addClass("show");
+            $(".pop-con").removeClass("show");
+            $("." + $(this).val() ).addClass("show");
+        });
+    }   
+
+    function clickRoleTitle(){
+        //$(".rule").toggleClass("on");
+        $(".right-side .guize").removeClass("hidden");
+        
+        var rules = $(".rule li");
+        var ruleString="";
+        $(".rule li").each(function(_, elem){
+            ruleString += $(elem).text().replace(/^\s*/ , "")+"\r\n";
+        });
+        $("#guize-content").val(ruleString);
+    }
+
+    function clickImage($this){    
+
+        $(".right-side .uploader").empty().removeClass("hidden").append($("#img-reset-tpl").text());
+        uploadimg('' , function( file ) {
+            $( '#'+file.id ).addClass('upload-state-done');
+            var oldImgSrc = $this.prop("src");
+            var imgPosition = $this.data("pos");
+            var newImgSrc = "generate/images/" + $("#fileList .info:last").text();
+            $this.prop("src" , newImgSrc);
+            $("#fileList").find(".smallimg").remove();
+        });
+
+        $(".uploader-list").append("<img class='smallimg' width=100 height=100 src='"+ $this.prop("src") +"'>");
+
+    }
+
+    $(".worktai")
+    .on("click", ".wrapper img , .rule , .btn-cnt .btn", function(e) {
+
+        var $this = $(this);
+        var $pop = $(".pop");
+        if($pop.hasClass("show")){
+            $pop.removeClass("show");
+        }
+
+        $(".right-side .tpl").addClass("hidden");
+
+        $(".wrapper img , .rule, .btn-cnt .btn")
+        .removeClass("mouse-click")
+        .filter(this)
+        .addClass("mouse-click");
+
+        if($this.hasClass("rule")){
+            clickRoleTitle();
+        }else if($this.hasClass("btn")){
+            clickRegisterButton();
+        }else{
+            clickImage($this);
+        }
+    });
+
+    $(".close").click(function(){                
+        $(".pop , .pop-con").removeClass("show");
+    });
+
+    $(".defaultSetting img").click(function(){
+        var $this = $(this);
+        $(".right-side .uploaderIconContainer").empty().append($("#img-reset-tpl").text());
+        uploadimg('' , function( file ) {                                        
+            var newImgSrc = "generate/images/" + $("#fileList .info:last").text();
+            $this.prop("src" , newImgSrc);                    
+        });
+    });
+
+
+    $(".poptxt").each(function (_, elem){                
+        elem.value = $($(elem).data("elemclass")).text();                
+    });
+
+    $(".poptxt").keyup(function(){
+        var newVal = $(this).val();
+        var selector = $(this).data("elemclass");
+        $(selector).text(newVal);
+    });
+
+});
+
+
+var myApp = angular.module('myApp', []);
+myApp.controller('ctrl1', ['$scope', '$http', function($scope, $http) {            
+    $scope.$watch('titleColor', function () {
+        $(".rule .title").css("background-color" , $scope.titleColor);
+    });
+
+    $scope.$watch('guizeContent', function () {
+
+        if(!$scope.guizeContent){
+            return ;
+        }
+        var rules = ($scope.guizeContent).split("\n");                
+        var x = rules.join("</li><li>");
+        console.log(x);
+        $(".rule .cnt").empty().append("<li>" +  rules.join("</li><li>") + "</li>");
+
+    });
+
+    
+
+    $scope.$watch('popupBtnColor', function () {
+        $(".btn").css({
+            "background-color" : $scope.popupBtnColor,
+            "box-shadow" : "1px 2px 0 0 " + $scope.popupBtnColor
+        });
+        
+    });   
+
+    $scope.$watch('backgroundColor', function () {
+        $(".wrapper").css("background-color" , $scope.backgroundColor);                
+    });        
+    
+}]);
